32b2b28af0b82cb14a0c9df819cf641389ed737d
Willie Harris <wdharri2@ncsu.edu>
2024-03-08 11:10:09 -0500

    fixed import from submodule

--- assembliss/qdb.py
+++ assembliss/qdb.py
@@ -1,4 +1,8 @@
 # simple setup
+import sys
+import os 
+# Add the ./Qdb of the project to the path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './Qdb')))
 from qdb import Qdb
 
-Qdb(["src/hello.s"], "rootfs/mipsel-linux-gnu", rr=True).interactive()
+Qdb(["Qdb/src/mips32el_hello"], "rootfs/mips32el_linux", rr=True).interactive()
