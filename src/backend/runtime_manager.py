eea25ffeb4f09e756f01959b95f08553b0ad21df
Willie Harris <willieharris3571@gmail.com>
2024-05-20 03:27:06 -0400

    Implemented tests and functionality for assembling and linking

--- src/backend/runtime_manager.py
+++ src/backend/runtime_manager.py
@@ -1,21 +1,60 @@
-class runtime_manager:
+# Copyright 2024 Willie D. Harris, Jr., Dr. Caio Batista de Melo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module provides a RuntimeManager class for managing the runtime 
+execution of ARMv8 assembly code.
+
+The RuntimeManager class provides methods for assembling, linking, running, 
+and debugging ARMv8 assembly code.
+"""
+import subprocess
+import os
+
+
+class RuntimeManager:
+    """
+    Manages the runtime execution of ARMv8 assembly code.
+    """
+
     def __init__(self, assembly_file: str):
         self.assembly_file = assembly_file
-        self.binary_file = None
+        self.obj_file = None
+        self.executable = None
 
     def assemble(self) -> str:
         """
         Assembles the ARMv8 assembly code into a binary file.
         Returns the path to the binary file.
         """
-        pass
+        executable_path = '/usr/bin/aarch64-linux-gnu-as'
+        # Replace file extension with .obj. Can be any extension.
+        self.obj_file = os.path.splitext(self.assembly_file)[0] + '.obj'
+        subprocess.run([executable_path, self.assembly_file,
+                        '-g', '-o', self.obj_file], check=True)
+        return self.obj_file
 
     def link(self) -> str:
         """
         Links the assembled code into an executable.
         Returns the path to the executable.
         """
-        pass
+        executable_path = '/usr/bin/aarch64-linux-gnu-ld'
+        # Remove the .obj extension.
+        self.executable = os.path.splitext(self.assembly_file)[0]
+        subprocess.run([executable_path, self.obj_file,
+                        '-o', self.executable], check=True)
+        return self.executable
 
     def run(self) -> str:
         """
---
+++ src/test/test_runtime_manager.py
@@ -0,0 +1,88 @@
+"""
+This module contains tests for the runtime manager using the pytest framework.
+"""
+import pytest
+from backend.runtime_manager import RuntimeManager
+import os
+
+
+class TestRuntimeManager:
+    """
+    This class contains unit tests for the runtime_manager class.
+    """
+
+    @pytest.fixture
+    def manager(self):
+        """
+        Returns a runtime manager object.
+
+        Parameters:
+        - filename (str): The name of the assembly file to be processed.
+
+        Returns:
+        - runtime_manager: An instance of the runtime_manager class.
+
+        """
+        return RuntimeManager("../sampleWorkspace/helloWorld.s")
+
+    def test_assemble(self, manager):
+        """
+        Test the assemble method of the runtime manager.
+
+        Args:
+            manager (RuntimeManager): The runtime manager instance.
+
+        Returns:
+            None
+        """
+        obj_file = manager.assemble()
+        if obj_file is None:
+            raise AssertionError("obj_file is None")
+
+        if os.path.exists(obj_file):
+            os.remove(obj_file)
+            
+    def test_link(self, manager):
+        """
+        Test the link method of the RuntimeManager class.
+
+        Args:
+            manager (RuntimeManager): An instance of the RuntimeManager class.
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: If the executable is None.
+        """
+        # Ensure the assembly step is performed first 
+        if not os.path.exists("../sampleWorkspace/helloWorld.obj"):
+            obj_file = manager.assemble()
+            if not os.path.exists("../sampleWorkspace/helloWorld.obj"):
+                raise AssertionError("helloWorld.obj is not created")
+            
+        executable = manager.link()
+        if executable is None:
+            raise AssertionError("executable is None")
+
+        # Teardown: Remove the created binary and executable files
+        if os.path.exists(obj_file):
+            os.remove(obj_file)
+        if os.path.exists(executable):
+            os.remove(executable)
+
+    # def test_run(self, manager):
+    #     """
+    #     Test the run method of the manager.
+
+    #     Args:
+    #         manager: The runtime manager object.
+
+    #     Returns:
+    #         None
+
+    #     Raises:
+    #         AssertionError: If the expected output is not found in the output
+    #     """
+    #     output = manager.run()
+    #     assert "expected output" in output
